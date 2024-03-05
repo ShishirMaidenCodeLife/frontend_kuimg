70fc7a17de676a0a327072494114a6b1f004ba54
Shishir <shishirpaudelofficial@gmail.com>
2024-03-05 11:22:14 +0545

    Feat: added recursive calling of train functionality based on val acc response for both (1.fetch data + CNN train and 2.fetch data + Gan + Train)

--- src/views/Page3View.vue
+++ src/views/Page3View.vue
@@ -1,189 +1,3 @@
-<!-- <template>
-  <main>
-    <UploadFile @newFile="onFileChange($event)" />
-    {{ tags }}
-    <div v-if="showStringField">
-      <input v-model="customString" placeholder="Enter a custom string" />
-      <button @click="submitCustomString">Submit</button>
-    </div>
-  </main>
-</template>
-
-<script>
-import UploadFile from '@/components/UploadFile.vue'
-import axios from 'axios'
-
-export default {
-  name: 'Page3View',
-  components: {
-    UploadFile,
-  },
-  data() {
-    return {
-      tags: [],
-      showStringField: false,
-      customString: '',
-    }
-  },
-  methods: {
-    onFileChange(file) {
-      this.fetchApi(file)
-    },
-
-    fetchApi(file) {
-      const data = new FormData();
-      data.append('img', file);
-
-      axios.post('/tag', data)
-        .then(response => {
-          if (response.data.tags === "We dont have model to recognise this, please train it first") {
-            this.showStringField = true;
-          } else {
-            this.showStringField = false;
-          }
-          this.tags = response.data.tags;
-        })
-        .catch(error => {
-          console.error('Error fetching tags:', error);
-        });
-    },
-
-    submitCustomString() {
-      // Send the string directly in the request body
-      const data = {
-        tag: this.customString,
-      };
-
-      axios.post('/autotag/img/fetch_train', data)
-        .then(response => {
-          // Handle the response as needed
-          console.log('Backend response:', response.data);
-          console.log(typeof response.data)
-          const returned_val_accuracy = response.data
-
-          if (returned_val_accuracy > 0.70) {
-            const registerData = {
-              template: "keras/MultiClassSingleTagKerasStandardModelTemplateA.py",
-              group: this.customString,  // Use customString as the group value
-              model_key: this.customString + '_model.zip',  
-            };
-                axios.post('/autotag/model/register', registerData).then(response => {
-                    console.log("model Registered!")
-                })
-            }
-        })
-        .catch(error => {
-          // Handle errors, if any
-          console.error('Error:', error);
-        });
-    },
-  }
-}
-</script> -->
-
-<!-- <template>
-  <main>
-    <UploadFile @newFile="onFileChange($event)" />
-    {{ tags }}
-    <div v-if="showStringField">
-      <input v-model="customString" placeholder="Please! Enter the label for the image HERE" />
-      <button @click="submitCustomString">Train</button>
-      <button v-if="showTrainButton" @click="runGanAndTrain">{{ trainButtonText }}</button>
-
-  </main>
-</template>
-
-<script>
-import UploadFile from '@/components/UploadFile.vue'
-import axios from 'axios'
-
-export default {
-  name: 'Page3View',
-  components: {
-    UploadFile,
-  },
-  data() {
-    return {
-      tags: [],
-      showStringField: false,
-      customString: '',
-      showTrainButton: false,
-      trainButtonText: '',
-    }
-  },
-  methods: {
-    onFileChange(file) {
-      this.fetchApi(file)
-    },
-
-    fetchApi(file) {
-      const data = new FormData();
-      data.append('img', file);
-
-      axios.post('/tag', data)
-        .then(response => {
-          if (response.data.tags === "We dont have model to recognise this, please train it first") {
-            this.showStringField = true;
-          } else {
-            this.showStringField = false;
-          }
-          this.tags = response.data.tags;
-        })
-        .catch(error => {
-          console.error('Error fetching tags:', error);
-        });
-    },
-
-    submitCustomString() {
-      const data = {
-        tag: this.customString,
-      };
-
-      axios.post('/autotag/img/fetch_train', data)
-        .then(response => {
-          const returned_val_accuracy = response.data;
-
-          if (returned_val_accuracy < 0.82) {
-
-            this.showTrainButton = true;
-            this.trainButtonText = "Run GAN and Train";
-          } else {
-            this.showTrainButton = false;
-          }
-
-          if (returned_val_accuracy > 0.86) {
-            const registerData = {
-              template: "keras/MultiClassSingleTagKerasStandardModelTemplateA.py",
-              group: this.customString,
-              model_key: this.customString + '_model.zip',
-            };
-
-            axios.post('/autotag/model/register', registerData).then(response => {
-              console.log("model Registered!")
-            })
-          }
-        })
-        .catch(error => {
-          console.error('Error:', error);
-        });
-    },
-
-    runGanAndTrain() {
-      // Implement the logic to run GAN and train
-      // You can use axios or another method to make the necessary API calls
-      
-      // For example:
-      // axios.post('/run_gan_and_train', { tag: this.customString })
-      //   .then(response => {
-      //     // Handle the response as needed
-      //   })
-      //   .catch(error => {
-      //     // Handle errors, if any
-      //   });
-    },
-  }
-}
-</script> -->
 
 <template>
   <main style="background-color: #fcfaff;">
@@ -216,7 +30,7 @@ export default {
     </div>
     <div v-if="modelSaveError">
       <p style="font-size: medium; font-weight: bold; color: rgb(227, 73, 73); ">
-        Model was not saved due to lower validation accuracy ie: {{ returnedValAccuracy }}
+        Model was not saved due to lower validation accuracy ie: {{ returnedValAccuracy }}. Re-Autotraining in Progress.
       </p>
       <button v-if="showTrainButton" @click="runGanAndTrain" style="width: auto;
         border: 2px solid #158be3;
@@ -226,6 +40,9 @@ export default {
 
         box-shadow: 0 2px 4px white(0, 0, 0, 0.1);
         color: white; padding: 10px;">{{ trainButtonText }}</button>
+    </div>
+    <div>
+    <UploadFile @newFile="onTestFileChange($event)" />
     </div>
   </main>
 </template>
@@ -279,58 +96,126 @@ export default {
         });
     },
 
-    submitCustomString() {
+        submitCustomString() {
+
+          // runGanAndTrain() {
+          const data = {
+            tag: this.customString,
+          };
+
+          const MAX_RETRIES = 2; // Set your desired maximum number of retries
+          let retryCount = 0;
+
+          const trainModel = () => {
+            // axios.post('/autotag/img/fetch_train', data)
+            axios.post('/autotag/img/fetch_train', data)
+              .then(response => {
+                this.returnedValAccuracy = response.data;
+
+                if (this.returnedValAccuracy > 0.85) {
+                  this.modelSaveSuccess = true;
+                  this.modelSaveError=false;
+                  const registerData = {
+                    template: "keras/MultiClassSingleTagKerasStandardModelTemplateA.py",
+                    group: this.customString,
+                    model_key: this.customString + '_model.zip',
+                  };
+
+                  axios.post('/autotag/model/register', registerData).then(response => {
+                    console.log("Model Registered!")
+                  });
+                } 
+                // else if (this.returnedValAccuracy >= 0.70 && this.returnedValAccuracy <= 0.85) {
+                //     // Code for accuracy between 0.70 and 0.85
+                //     // Add your specific handling for this range
+            
+                // } 
+                
+                else {
+                  
+                  this.modelSaveSuccess=false;
+                  this.modelSaveError=true;
+                  // Check if the maximum number of retries is reached
+                  if (retryCount < MAX_RETRIES) {
+                    retryCount++;
+                    // Recursive call with a delay (e.g., 1 second) to avoid rapid retries
+                    setTimeout(trainModel, 1000);
+                  } else {
+                    this.modelSaveError = true;
+                    console.error('Maximum number of retries reached. Model not saved.');
+                    this.showTrainButton=true;
+                    this.trainButtonText="Run GAN and Train";
+                  }
+                }
+              })
+              .catch(error => {
+                this.modelSaveError = true;
+                console.error('Error:', error);
+              });
+          };
+
+          trainModel(); // Initial call to start the process
+    },
+
+    runGanAndTrain() {
       const data = {
         tag: this.customString,
       };
 
-      axios.post('/autotag/img/fetch_train', data)
+      const MAX_RETRIES = 2;
+      let retryCount = 0;
+
+      const trainGanCnn = () => {
+        // axios.post('/autotag/ml/train/gan_cnn', data)
+        axios.post('/autotag/ml/train/fetch_gan_cnn', data)
         .then(response => {
-          this.returnedValAccuracy = response.data;
+                this.returnedValAccuracy = response.data;
 
-          if (this.returnedValAccuracy < 0.82) {
-            this.modelSaveSuccess = false;
+                if (this.returnedValAccuracy > 0.85) {
+                  this.modelSaveSuccess = true;
+                  this.modelSaveError=false;
+                  const registerData = {
+                    template: "keras/MultiClassSingleTagKerasStandardModelTemplateA.py",
+                    group: this.customString,
+                    model_key: this.customString + '_model.zip',
+                  };
+                  console.log("GAN and Train successful!");
+
+                  axios.post('/autotag/model/register', registerData).then(response => {
+                    console.log("Model Registered!")
+                  });      
+                } 
+                
+                // else if (this.returnedValAccuracy >= 0.70 && this.returnedValAccuracy <= 0.85) {
+                //     // Code for accuracy between 0.70 and 0.85
+                //     // Add your specific handling for this range
+            
+                // } 
+                else {
+              // Validation loss is not satisfactory
+                  this.modelSaveSuccess=false;
+                  this.modelSaveError=true;
+                  // Check if the maximum number of retries is reached
+                  if (retryCount < MAX_RETRIES) {
+                    retryCount++;
+                    // Recursive call with a delay (e.g., 1 second) to avoid rapid retries
+                    setTimeout(trainGanCnn, 1000);
+                  } else {
+                    this.modelSaveError = true;
+                    console.error('Maximum number of retries reached. Model not saved.');
+              }
+            }
+          })
+          .catch(error => {
+            // Handle errors
             this.modelSaveError = true;
-            this.showTrainButton = true;
-            this.trainButtonText = "Run GAN and Train";
-          } else {
-            this.showTrainButton = false;
-          }
+            console.error('Error during GAN and Train:', error);
+          });
+      };
 
-          if (this.returnedValAccuracy > 0.86) {
-            this.modelSaveSuccess = true;
-            const registerData = {
-              template: "keras/MultiClassSingleTagKerasStandardModelTemplateA.py",
-              group: this.customString,
-              model_key: this.customString + '_model.zip',
-            };
-
-            axios.post('/autotag/model/register', registerData).then(response => {
-              console.log("model Registered!")
-            })
-          } else {
-            this.modelSaveSuccess = false;
-          }
-        })
-        .catch(error => {
-          this.modelSaveError = true;
-          console.error('Error:', error);
-        });
-    },
-
-    runGanAndTrain() {
-      // logic to run GAN and train
-      // We can use axios or another method to make the necessary API calls
-      
-      // For example:
-      // axios.post('/run_gan_and_train', { tag: this.customString })
-      //   .then(response => {
-      //     // Handle the response as needed
-      //   })
-      //   .catch(error => {
-      //     // Handle errors, if any
-      //   });
+      trainGanCnn(); // Initial call to start the process
     },
   }
 }
 </script>
+
