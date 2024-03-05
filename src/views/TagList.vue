ffbb1a496b74f29537865c15efe078c1463d96e7
Shishir <shishirpaudelofficial@gmail.com>
2024-03-05 12:59:44 +0545

    changed the TagList display in the front end
    display will have bulleted tags list for trained models

--- src/views/TagList.vue
+++ src/views/TagList.vue
@@ -1,4 +1,4 @@
-<template>  
+<!-- <template>  
     <main>
       {{ tags }}
     </main>
@@ -53,4 +53,59 @@
     
     }
   }
-  </script> 
+  </script>  -->
+
+  <template>
+    <main>
+      <ul>
+        <li v-for="(tags, index) in tags" :key="index">
+          {{ index + 1 }}. {{ tags.join(', ') }}
+        </li>
+      </ul>
+    </main>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  
+  export default {
+    name: 'TagsList',
+    created() {
+      axios.get(`/status`)
+        .then(response => (
+          this.keys = response.data
+        ));
+    },
+    data() {
+      return {
+        keys: null,
+      };
+    },
+    computed: {
+      tags() {
+        let items = [];
+  
+        if (this.keys) {
+          var keys = Object.keys(this.keys);
+          for (var i = 0; i < keys.length; i++) {
+            items.push(this.getTags(this.keys[keys[i]]));
+          }
+        }
+        return items;
+      },
+    },
+    methods: {
+      getTags(value) {
+        let temp = value.split("tags")[1]
+        temp = temp.replace(':', '')
+        temp = temp.replace('}', '')
+        temp = temp.replace('[', '')
+        temp = temp.replace(']', '')
+        temp = temp.slice(1)
+        temp = temp.split(",")
+        return temp;
+      },
+    },
+  };
+  </script>
+  
